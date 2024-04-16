6d350340a8538f921d7d6129b006322ba6801732
Ngong_xj55@top-academy.ru <Ngong_xj55@top-academy.ru>
2024-04-16 21:19:12 +0300

    FunctionOverloading

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -6,9 +6,15 @@ using std::endl;
 #define tab "\t"
 
 void FillRand(int arr[], const int n);
+void FillRand(double arr[], const int n);
+
 void Print(  int arr[], const int n);
+void Print(  double arr[], const int n);
+
 void Sort(int arr[], const int n);
+
 int Sum(  int arr[], const int n);
+double Sum(  double arr[], const int n);
 double Avg(  int arr[], const int n);
 int minValueIn(  int arr[], const int n);
 int maxValueIn(int arr[], const int n);
@@ -29,12 +35,19 @@ void main()
 	cout << "minValue in array: " << minValueIn(arr,n) << endl;
 	cout << "maxValue in array: " << maxValueIn(arr,n) << endl;
 	int number_of_shifts;
-	cout << "Enter number of shifts: "; cin >> number_of_shifts;
-	shiftLeft(arr, n, number_of_shifts);
-	Print(arr, n);
-	cout << "Enter number of shift: "; cin >> number_of_shifts;
-	shiftRight(arr, n, number_of_shifts);
-	Print(arr, n);
+	//cout << "Enter number of shifts: "; cin >> number_of_shifts;
+	//shiftLeft(arr, n, number_of_shifts);
+	//Print(arr, n);
+	//cout << "Enter number of shift: "; cin >> number_of_shifts;
+	//shiftRight(arr, n, number_of_shifts);
+	//Print(arr, n);
+	const int SIZE = 8;
+	double brr[SIZE];
+	FillRand(brr, SIZE);
+	Print(brr, SIZE);
+	cout << "Sum of array elements: " << Sum(brr, SIZE) << endl;
+	//cout << "minValue in array: " << minValueIn(brr,SIZE) << endl;
+	//cout << "maxValue in array: " << maxValueIn(brr, SIZE) << endl;
 }
  
 void FillRand(int arr[], const int n)
@@ -44,7 +57,22 @@ void FillRand(int arr[], const int n)
 		arr[i] = rand() % 100;
 	}
 }
+void FillRand(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
 void Print(  int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+void Print(double arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -68,6 +96,15 @@ void Sort(int arr[], const int n)
 	}
 }
 int Sum(  int arr[], const int n)
+{
+	int Sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		Sum += arr[i];
+	}
+	return Sum;
+}
+double Sum(double arr[], const int n)
 {
 	int Sum = 0;
 	for (int i = 0; i < n; i++)
