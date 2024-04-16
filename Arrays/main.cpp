4d1659f9d120add33660101cc74e8f6961c726ef
Ngong_xj55@top-academy.ru <Ngong_xj55@top-academy.ru>
2024-04-16 20:39:44 +0300

    HomeWork done

---
+++ Arrays/main.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+
+void FillRand(int arr[], const int n);
+void Print(  int arr[], const int n);
+void Sort(int arr[], const int n);
+int Sum(  int arr[], const int n);
+double Avg(  int arr[], const int n);
+int minValueIn(  int arr[], const int n);
+int maxValueIn(int arr[], const int n);
+void shiftLeft(int arr[], const int n, int number_of_shifts);
+void shiftRight(int arr[], const int n, int number_of_shifts);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+	FillRand(arr, n);
+	Print(arr, n);
+	Sort(arr, n);
+	Print(arr, n);
+	cout << "Sum of array elements: " << Sum(arr, n) << endl;
+	cout << "Avarage of array elements: " << Avg(arr, n) << endl;
+	cout << "minValue in array: " << minValueIn(arr,n) << endl;
+	cout << "maxValue in array: " << maxValueIn(arr,n) << endl;
+	int number_of_shifts;
+	cout << "Enter number of shifts: "; cin >> number_of_shifts;
+	shiftLeft(arr, n, number_of_shifts);
+	Print(arr, n);
+	cout << "Enter number of shift: "; cin >> number_of_shifts;
+	shiftRight(arr, n, number_of_shifts);
+	Print(arr, n);
+}
+ 
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+void Print(  int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+void Sort(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if(arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+int Sum(  int arr[], const int n)
+{
+	int Sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		Sum += arr[i];
+	}
+	return Sum;
+}
+double Avg(  int arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
+}
+int minValueIn(int arr[], const int n)
+{
+	int min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+int maxValueIn(int arr[], const int n)
+{
+	int max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+}
+void shiftLeft(int arr[], const int n, int number_of_shifts)
+{
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[0];
+		for (int i = 1; i < n; i++)
+		{
+			arr[i - 1] = arr[i];
+		}
+		arr[n - 1] = buffer;
+	}
+}
+void shiftRight(int arr[], const int n, int number_of_shifts)
+{
+	shiftLeft(arr, n, number_of_shifts);
+}
--- Functions/main.cpp
+++ Functions/main.cpp
@@ -1,8 +1,88 @@
+
 #include<iostream>
 using namespace std;
 
+int Sum(int a , int b  );
+
+int Diff(int a, int b);
+int Prod(int a, int b);
+double Quot(int a, int b = 0);
+double Factorial(int n);
+double Power(double a, int n);
+#define CLassWork
+//#define Factorial
+//#define Power
 void main()
 {
+ 
+#ifdef CLassWork
 	setlocale(LC_ALL, "");
 	cout << "Hello Funcions" << endl;
+	int a, b;
+	cout << "Enter two numbers: "; cin >> a >> b;
+	int c = Sum(a , b);
+	cout << a << " + " << b << " = " << c << endl;
+	cout << a << " - " << b << " = " << Diff(a, b) << endl;
+	cout << a << " * " << b << " = " << Prod(a, b) << endl;
+	cout << a << " / " << b << " = " << Quot(a) << endl;
+
+		 
+#endif // CLassWork
+#ifdef Factorial
+	double Factorial(int n)
+	{
+		double f = 1;
+		for (int i = 0; i < n; i++)
+		{
+			f *= 1;
+		}
+		return f;
+	}
+#endif // Factorial 
+
+#ifdef Power
+
+	int a;
+	int n;
+	cout << " Enter the degree base: "; cin >> a;
+	cout << "Enter the exponent :"; cin >> n;
+	cout << a << " ^ " << n << " = " << Power(a) << endl;
+#endif // Power
+
+
+
+}
+
+int Sum(int a, int b)
+{
+	int c = a + b;
+	return c;
+}
+int Diff(int a, int b)
+{
+	return a - b;
+}
+int Prod(int a, int b)
+{
+	return a * b;
+}
+double Quot(int a, int b)
+{
+	return (double)a / b;
+	 
+}
+ 
+double Power(double a, int n)
+{
+	double N = 1;
+	if (n < 0)
+	{
+		n = -n;
+		a = 1 / a;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		N *= a;
+	}
+	return N;
 }
