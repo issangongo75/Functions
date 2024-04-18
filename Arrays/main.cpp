d1738c852b0312efed80bf3f3997e7d9aa74b1f8
Ngong_xj55@top-academy.ru <Ngong_xj55@top-academy.ru>
2024-04-18 21:44:15 +0300

    2D overloading in progress

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -15,20 +15,25 @@ void FillRand(double arr[], const int n);
 void FillRand(char arr[], const int n);
 void FillRand(int arr[ROWS][COLS], const int Rows, const int COLS);
 
-void Print(  int arr[], const int n);
-void Print(  double arr[], const int n);
-void Print(  char arr[], const int n);
+void Print(int arr[], const int n);
+void Print(double arr[], const int n);
+void Print(char arr[], const int n);
+void Print(int arr[ROWS][COLS], const int ROWS, const int COLS);
 
 void Sort(int arr[], const int n);
 void Sort(double arr[], const int n);
 
-int Sum(  int arr[], const int n);
-double Sum(  double arr[], const int n);
+int Sum(int arr[], const int n);
+double Sum(double arr[], const int n);
 int Sum(char arr[], const int n);
+int Sum(int arr[ROWS][COLS], const int ROWS, const int COLS);
+double Avg(int arr[], const int n);
+double Avg(int arr[ROWS][COLS], const int ROWS, const int COLS);
+int minValueIn(int arr[], const int n);
+double minValueIn(double arr[], const int n);
 
-double Avg(  int arr[], const int n);
-int minValueIn(  int arr[], const int n);
 int maxValueIn(int arr[], const int n);
+double maxValueIn(double arr[], const int n);
 void shiftLeft(int arr[], const int n, int number_of_shifts);
 void shiftRight(int arr[], const int n, int number_of_shifts);
 
@@ -43,8 +48,8 @@ void main()
 	Print(i_arr, I_SIZE);
 	cout << "Sum of array elements: " << Sum(i_arr, I_SIZE) << endl;
 	cout << "Avarage of array elements: " << Avg(i_arr, I_SIZE) << endl;
-	cout << "minValue in array: " << minValueIn(i_arr,I_SIZE) << endl;
-	cout << "maxValue in array: " << maxValueIn(i_arr,I_SIZE) << endl;
+	cout << "minValue in array: " << minValueIn(i_arr, I_SIZE) << endl;
+	cout << "maxValue in array: " << maxValueIn(i_arr, I_SIZE) << endl;
 	int number_of_shifts;
 	//cout << "Enter number of shifts: "; cin >> number_of_shifts;
 	//shiftLeft(arr, n, number_of_shifts);
@@ -57,11 +62,11 @@ void main()
 	FillRand(d_arr, D_SIZE);
 	Print(d_arr, D_SIZE);
 	cout << "Sum of array elements: " << Sum(d_arr, D_SIZE) << endl;
-	//cout << "minValue in array: " << minValueIn(brr,SIZE) << endl;
-	//cout << "maxValue in array: " << maxValueIn(brr, SIZE) << endl;
+	cout << "minValue in array: " << minValueIn(d_arr, D_SIZE) << endl;
+	cout << "maxValue in array: " << maxValueIn(d_arr, D_SIZE) << endl;
 	Sort(d_arr, D_SIZE);
 	Print(d_arr, D_SIZE);
-	
+
 	cout << delimiter << endl;
 
 	const int C_SIZE = 11;
@@ -74,8 +79,11 @@ void main()
 
 	int i_arr_2[ROWS][COLS];
 	FillRand(i_arr_2, ROWS, COLS);
+	Print(i_arr_2, ROWS, COLS);
+	cout << "Sum of elements: " << Sum(i_arr_2, ROWS, COLS) << endl;
+	cout << "Aritmetic_mean_of_array_elements : " << Avg(i_arr_2, ROWS, COLS) << endl;
 }
- 
+
 void FillRand(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
@@ -104,11 +112,11 @@ void FillRand(int arr[ROWS][COLS], const int Rows, const int COLS)
 	{
 		for (int j = 0; j < COLS; j++)
 		{
-			arr[i][j] = rand()  % 100;
+			arr[i][j] = rand() % 100;
 		}
 	}
 }
-void Print(  int arr[], const int n)
+void Print(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -132,13 +140,25 @@ void Print(char arr[], const int n)
 	}
 	cout << endl;
 }
+void Print(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
 void Sort(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = i + 1; j < n; j++)
 		{
-			if(arr[j] < arr[i])
+			if (arr[j] < arr[i])
 			{
 				int buffer = arr[i];
 				arr[i] = arr[j];
@@ -154,7 +174,7 @@ void Sort(double arr[], const int n)
 		for (int j = i + 1; j < n; j++)
 		{
 			if (arr[j] < arr[i])
-			{ 
+			{
 				double buffer = arr[i];
 				arr[i] = arr[j];
 				arr[j] = buffer;
@@ -189,18 +209,34 @@ int Sum(char arr[], const int n)
 	}
 	return Sum;
 }
+int Sum(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	int sum = 0;
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			sum += arr[i][j];
+		}
+	}
+	return sum;
+}
 
-double Avg(  int arr[], const int n)
+double Avg(int arr[], const int n)
 {
 	return (double)Sum(arr, n) / n;
 }
 double Avg(double arr[], const int n)
-{ 
+{
 	return Sum(arr, n) / n;
 }
 double Avg(char arr[], const int n)
 {
 	return Sum(arr, n) / n;
+}
+double Avg(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	return(double)Sum(arr, ROWS, COLS) / ROWS / COLS;
 }
 
 int minValueIn(int arr[], const int n)
